32f005df672ec86652e924cbfe8844723dcd730d
Engel Chavarria <chavarriaengel97@gmail.com>
2025-09-26 09:52:08 -0600

    gile gitignore

--- Farmacia VitaCare/obj/Debug/net8.0-windows/Farmacia VitaCare.AssemblyInfo.cs
+++ Farmacia VitaCare/obj/Debug/net8.0-windows/Farmacia VitaCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmacia VitaCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4634128d54ed6afd0bd9e174c04ea2c0f4d513")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmacia VitaCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmacia VitaCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
