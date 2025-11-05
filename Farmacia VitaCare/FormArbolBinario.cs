1558224fe8dc56ae1118d8aab57f1c57aeb5a0f2
Engel Chavarria <chavarriaengel97@gmail.com>
2025-11-05 10:04:21 -0600

    window form corregido

--- Farmacia VitaCare/FormArbolBinario.cs
+++ Farmacia VitaCare/FormArbolBinario.cs
@@ -16,5 +16,10 @@ namespace Farmacia_VitaCare
         {
             InitializeComponent();
         }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
